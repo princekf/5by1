02b98fba581a480c138a1d93206ecfa78183c9df
Daniel <danielcgrace1998@gmail.com>
2021-06-22 20:14:08 +0530

    Create-product: within this component, Add some aditional modification,validation and  mobile friendly.

--- ui/src/app/modules/main/inventory/product/create-product/create-product.component.spec.ts
+++ ui/src/app/modules/main/inventory/product/create-product/create-product.component.spec.ts
@@ -1,25 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreateItemComponent } from './create-product.component';
+ 
 
-describe('CreateItemComponent', () => {
-  let component: CreateItemComponent;
-  let fixture: ComponentFixture<CreateItemComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CreateItemComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreateItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+ 
+   
+    
