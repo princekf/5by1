a7f112bec0c7663923e609fc9c3b287cbb9ba352
Murugan <murugan.c@inreality.com>
2021-08-29 21:46:45 +0530

    Revert "Merge branch 'feature/transfer-CURD-operation' into develop"
    This reverts commit 56a353114e0c5d03dec6bc526a4262280a9e7dcb.

--- ui/src/app/modules/main/inventory/transfer/transfer.module.ts
+++ ui/src/app/modules/main/inventory/transfer/transfer.module.ts
@@ -4,24 +4,12 @@ import { ListTransferComponent } from './list-transfer/list-transfer.component';
 import { CreateTransferComponent } from './create-transfer/create-transfer.component';
 import { TransferRoutingModule } from './transfer.routing.module';
 import { DeleteTransferComponent } from './delete-transfer/delete-transfer.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import { ToolBarModule } from '../../tool-bar/tool-bar.module';
-import { DataTableModule } from '../../data-table/data-table.module';
-import {MatTableModule} from '@angular/material/table';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-import {MatSortModule} from '@angular/material/sort';
+
 
 @NgModule({
   declarations: [ ListTransferComponent, CreateTransferComponent, DeleteTransferComponent ],
   imports: [
-    CommonModule, TransferRoutingModule, MatSelectModule, MatFormFieldModule, ReactiveFormsModule,
-    FormsModule, MatInputModule, MatButtonModule, MatTableModule, MatSortModule,
-    NgxSkeletonLoaderModule, ToolBarModule,
-    DataTableModule,
+    CommonModule, TransferRoutingModule
   ]
 })
 export class TransferModule { }
