1f4d57c9421cb957c4f2becbda53bd92566f5fbc
Murugan <murugan.c@inreality.com>
2021-08-30 12:12:52 +0530

    latest-code-with-date picker and transfer-CURD function

--- ui/src/app/modules/main/inventory/transfer/list-transfer/list-transfer.component.spec.ts
+++ ui/src/app/modules/main/inventory/transfer/list-transfer/list-transfer.component.spec.ts
@@ -3,23 +3,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListTransferComponent } from './list-transfer.component';
 
 describe('ListTransferComponent', () => {
+
   let component: ListTransferComponent;
   let fixture: ComponentFixture<ListTransferComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async() => {
+
     await TestBed.configureTestingModule({
       declarations: [ ListTransferComponent ]
     })
-    .compileComponents();
+      .compileComponents();
+
   });
 
   beforeEach(() => {
+
     fixture = TestBed.createComponent(ListTransferComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
+
     expect(component).toBeTruthy();
+
   });
+
 });
