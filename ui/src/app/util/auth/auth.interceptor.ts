a95c65cabdb92b358cef6c82fa2ec4184f38740f
Prince Francis <princekfrancis@gmail.com>
2021-10-12 21:07:05 +0530

    Handling session expired.

--- ui/src/app/util/auth/auth.interceptor.ts
+++ ui/src/app/util/auth/auth.interceptor.ts
@@ -1,30 +1,47 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { ACCESS_TOKEN_ID } from '@shared/Constants';
+import { Observable, throwError } from 'rxjs';
+import { ACCESS_TOKEN_ID, HTTP_RESPONSE_CODE } from '@shared/Constants';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  // eslint-disable-next-line class-methods-use-this
+  constructor(private readonly toastr: ToastrService) { }
+
   intercept(req: HttpRequest<unknown>,
     next: HttpHandler): Observable<HttpEvent<unknown>> {
 
+    let reqN = req;
     const idToken = localStorage.getItem(ACCESS_TOKEN_ID);
 
     if (idToken) {
 
-      const cloned = req.clone({
+      reqN = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${idToken}`)
       });
 
-      return next.handle(cloned);
-
     }
 
-    return next.handle(req);
+    return next.handle(reqN).pipe(
+      catchError((error: HttpErrorResponse) => {
+
+        if (error.status === HTTP_RESPONSE_CODE.FORBIDDEN_USER) {
+
+          this.toastr.error('Your session might have expired.<br/><a href=\"/login\">Please login again</a>', 'Session expired', {
+            enableHtml: true,
+            closeButton: true,
+            timeOut: 10000
+          });
+
+        }
+        return throwError(error);
+
+      }), map((event: HttpEvent<unknown>) => event)
+    );
 
   }
 
