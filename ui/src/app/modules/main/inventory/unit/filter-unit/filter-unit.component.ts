2f51f28b0ad51937ecf47ba603884d8646af7753
Prince Francis <princekfrancis@gmail.com>
2021-10-12 10:52:18 +0530

    Integration of Unit filtering with backend started

--- ui/src/app/modules/main/inventory/unit/filter-unit/filter-unit.component.ts
+++ ui/src/app/modules/main/inventory/unit/filter-unit/filter-unit.component.ts
@@ -1,4 +1,7 @@
 import { Component, } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QueryData } from '@shared/util/query-data';
 
 @Component({
   selector: 'app-filter-unit',
@@ -7,13 +10,55 @@ import { Component, } from '@angular/core';
 })
 export class FilterUnitComponent {
 
-  selectedone = 'one';
-
   selectedtwo = 'one';
 
   selectedthree = 'one';
 
   selectedfour = 'one';
 
+  queryParams:QueryData = { };
+
+  filterForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    nameType: new FormControl('^'),
+    code: new FormControl(''),
+    codeType: new FormControl('^'),
+  });
+
+  constructor(private router:Router,
+    private activatedRoute : ActivatedRoute) { }
+
+  ngAfterViewInit():void {
+
+    this.activatedRoute.queryParams.subscribe((value) => {
+
+      this.queryParams = { ...value };
+
+    });
+
+  }
+
+  filterItems = ():void => {
+
+    const whereAnds:Array<unknown> = [];
+    if (this.filterForm.controls.name.value) {
+
+      whereAnds.push(
+        {name: {like: `${this.filterForm.controls.nameType.value}${this.filterForm.controls.name.value}`,
+          options: 'i'}}
+      );
+
+    }
+    if (this.filterForm.controls.code.value) {
+
+      whereAnds.push(
+        {code: {like: `${this.filterForm.controls.codeType.value}${this.filterForm.controls.code.value}`,
+          options: 'i'}}
+      );
+
+    }
+    this.router.navigate([], { queryParams: {whereS: JSON.stringify({and: whereAnds})} });
+
+  };
 
 }
