0790a7fddebe7c60c32b6184517ded1c6f67f8b0
Prince Francis <princekfrancis@gmail.com>
2022-11-07 14:17:33 +0530

    Update ledger-group.controller.ts and ledger-group.service.ts

--- server/src/controllers/ledger-group.controller.ts
+++ server/src/controllers/ledger-group.controller.ts
@@ -1,7 +1,6 @@
 import { Count, CountSchema, Filter, FilterExcludingWhere, repository, Where } from '@loopback/repository';
-import { post, param, get, getModelSchemaRef, patch, put, del, requestBody, response, HttpErrors, Request, Response, RestBindings } from '@loopback/rest';
+import { post, param, get, getModelSchemaRef, patch, put, del, requestBody, response, Request, Response, RestBindings } from '@loopback/rest';
 import {LedgerGroup} from '../models/ledger-group.model';
-import {LedgerGroupRepository} from '../repositories/ledger-group.repository';
 import { LEDGER_GROUP_API} from '@shared/server-apis';
 import { authenticate } from '@loopback/authentication';
 import { authorize } from '@loopback/authorization';
@@ -11,22 +10,18 @@ import { inject, intercept } from '@loopback/context';
 import { ValidateLedgerGroupInterceptor } from '../interceptors/validate-ledgergroup.interceptor';
 import { DenyDeletionOfDefaultLedgerGroup } from '../interceptors';
 import { LedgerGroupWithParents } from '../models/ledger-group-with-parents.model';
-import xlsx from 'xlsx';
 import { BindingKeys } from '../binding.keys';
 import { LedgerGroupService, ProfileUser } from '../services';
 import { FileUploadHandler } from '../types';
 import {SecurityBindings} from '@loopback/security';
 import { FinYearRepository } from '../repositories';
 import { LedgerGroupImport } from '../utils/ledgergroup-import-spec';
-import { Save } from '../utils/save-spec';
 import { service } from '@loopback/core';
 @authenticate('jwt')
 @authorize(adminAndUserAuthDetails)
 export class LedgerGroupController {
 
   constructor(
-    @repository(LedgerGroupRepository)
-    public ledgerGroupRepository : LedgerGroupRepository,
     @service(LedgerGroupService) private ledgerGroupService: LedgerGroupService,
   ) {}
 
@@ -53,7 +48,7 @@ export class LedgerGroupController {
   ): Promise<LedgerGroup> {
 
 
-    const ledgergroupR = await this.ledgerGroupRepository.create(ledgerGroup);
+    const ledgergroupR = await this.ledgerGroupService.create(ledgerGroup);
     return ledgergroupR;
 
   }
@@ -69,7 +64,7 @@ export class LedgerGroupController {
     @param.where(LedgerGroup) where?: Where<LedgerGroup>,
   ): Promise<Count> {
 
-    const countR = await this.ledgerGroupRepository.count(where);
+    const countR = await this.ledgerGroupService.count(where);
     return countR;
 
   }
@@ -97,47 +92,6 @@ export class LedgerGroupController {
 
   }
 
-  private fetchChilds = async(lgCodes: Array<string>):Promise<Array<LedgerGroup>> => {
-
-    const pQuery = await this.ledgerGroupRepository.execute(this.ledgerGroupRepository.modelClass.name, 'aggregate', [
-      {
-        '$graphLookup': {
-          'from': 'LedgerGroup',
-          'startWith': '$parentId',
-          'connectFromField': 'parentId',
-          'connectToField': '_id',
-          'as': 'parents',
-        }
-      },
-      {
-        '$addFields': {
-          'id': '$_id',
-        }
-      },
-      {
-        '$addFields': {
-          'parents': {
-            '$filter': {
-              'input': '$parents',
-              'cond': { '$in': [ '$$this.code', lgCodes ] }
-            }
-          }
-        }
-      },
-      {'$match': { '$or': [
-        {'parents': {'$exists': true,
-          '$not': {'$size': 0}}},
-        {'code': {'$in': lgCodes}}
-      ]}},
-    ]);
-
-
-    const lgsR = <Array<LedgerGroup>> await pQuery.toArray();
-    return lgsR;
-
-  }
-
-
   @get(`${LEDGER_GROUP_API}/childs`)
   @response(200, {
     description: 'Childs of ledger groups',
@@ -158,20 +112,7 @@ export class LedgerGroupController {
     @param.where(LedgerGroup) where?: Where<LedgerGroup>,
   ): Promise<LedgerGroup[]> {
 
-    if (!where) {
-
-      throw new HttpErrors.Conflict('Invalid parameter : LedgerGroup ids are required');
-
-    }
-    const whereC = where as {code: {inq: Array<string>}};
-    if (!whereC.code || !whereC.code.inq || whereC.code.inq.length < 1) {
-
-      throw new HttpErrors.Conflict('Invalid parameter : LedgerGroup ids are required');
-
-    }
-    const lgsR = await this.fetchChilds(whereC.code.inq);
-
-
+    const lgsR = await this.ledgerGroupService.childs(where);
     return lgsR;
 
   }
@@ -195,7 +136,7 @@ export class LedgerGroupController {
     @param.where(LedgerGroup) where?: Where<LedgerGroup>,
   ): Promise<Count> {
 
-    const countR = await this.ledgerGroupRepository.updateAll(ledgerGroup, where);
+    const countR = await this.ledgerGroupService.updateAll(ledgerGroup, where);
     return countR;
 
   }
@@ -216,7 +157,7 @@ export class LedgerGroupController {
     @param.filter(LedgerGroup, {exclude: 'where'}) filter?: FilterExcludingWhere<LedgerGroup>
   ): Promise<LedgerGroup> {
 
-    const lgR = await this.ledgerGroupRepository.findById(id, filter);
+    const lgR = await this.ledgerGroupService.findById(id, filter);
     return lgR;
 
   }
@@ -240,7 +181,7 @@ export class LedgerGroupController {
       ledgerGroup: LedgerGroup,
   ): Promise<void> {
 
-    await this.ledgerGroupRepository.updateById(id, ledgerGroup);
+    await this.ledgerGroupService.updateById(id, ledgerGroup);
 
   }
 
@@ -256,7 +197,7 @@ export class LedgerGroupController {
     @requestBody() ledgerGroup: LedgerGroup,
   ): Promise<void> {
 
-    await this.ledgerGroupRepository.replaceById(id, ledgerGroup);
+    await this.ledgerGroupService.replaceById(id, ledgerGroup);
 
   }
 
@@ -269,7 +210,7 @@ export class LedgerGroupController {
     ...adminAndUserAuthDetails})
   async deleteById(@param.path.string('id') id: string): Promise<void> {
 
-    await this.ledgerGroupRepository.deleteById(id);
+    await this.ledgerGroupService.deleteById(id);
 
   }
 
@@ -285,94 +226,11 @@ export class LedgerGroupController {
     @param.where(LedgerGroup) where?: Where<LedgerGroup>,
   ): Promise<Count> {
 
-    if (!where) {
-
-      throw new HttpErrors.Conflict('Invalid parameter : LedgerGroup ids are required');
-
-    }
-    const whereC = where as {id: {inq: Array<string>}};
-    if (!whereC.id || !whereC.id.inq || whereC.id.inq.length < 1) {
-
-      throw new HttpErrors.Conflict('Invalid parameter : LedgerGroup ids are required');
-
-    }
-
-    const count = await this.ledgerGroupRepository.deleteAll(where);
+    const count = await this.ledgerGroupService.deleteAll(where);
     return count;
 
   }
 
-  private saveUploadedFile = (fileUploadHandler: FileUploadHandler, request: Request, response2: Response) =>
-    new Promise<Save>((resolve, reject) => {
-
-      fileUploadHandler(request, response2, (err: string) => {
-
-        if (err) {
-
-          reject(err);
-
-        } else {
-
-          resolve(LedgerGroupController.getFilesAndFields(request));
-
-        }
-
-
-      });
-
-    })
-
-
-    private createLedgerGroup = async(ledgergroupData:Array<LedgerGroupImport>,
-      uProfile: ProfileUser, finYearRepository : FinYearRepository)
-      :Promise<LedgerGroupImport[]> => {
-
-      const ledger:Array<LedgerGroupImport> = [];
-
-
-      for (const lgData of ledgergroupData) {
-
-
-        const code = lgData.Code;
-        const name = lgData.Name;
-        const parentCode = lgData.ParentCode;
-        const details = lgData.Details;
-        let parentId;
-        if (parentCode) {
-
-          const pLGroup = await this.ledgerGroupRepository.findOne({where: {code: parentCode}});
-          parentId = pLGroup?.id;
-
-        } else {
-
-          ledger.push(lgData);
-
-
-          continue;
-
-        }
-        const finYear = await finYearRepository.findOne({where: {code: {regexp: `/^${uProfile.finYear}$/i`}}});
-
-
-        if (!finYear) {
-
-          throw new HttpErrors.UnprocessableEntity('Please select a proper financial year.');
-
-
-        }
-
-        await this.ledgerGroupRepository.create({
-          code,
-          name,
-          parentId,
-          details,
-        });
-
-
-      }
-      return ledger;
-
-    }
 
   @post(`${LEDGER_GROUP_API}/import`, {
     responses: {
@@ -388,63 +246,18 @@ export class LedgerGroupController {
       },
     },
   })
-    async importLedgerGroup(
+  async importLedgerGroup(
     @requestBody.file()
       request: Request,
     @inject(RestBindings.Http.RESPONSE) response2: Response,
     @inject(BindingKeys.FILE_UPLOAD_SERVICE) fileUploadHandler: FileUploadHandler,
     @inject(SecurityBindings.USER) uProfile: ProfileUser,
     @repository(FinYearRepository) finYearRepository : FinYearRepository,
-    ): Promise<LedgerGroupImport[]> {
+  ): Promise<LedgerGroupImport[]> {
 
-      await this.saveUploadedFile(fileUploadHandler, request, response2);
-      const [ fileDetails ] = request.files as Array<{path: string}>;
-      const savedFilePath = fileDetails.path;
-      const workBook = xlsx.readFile(savedFilePath);
-      const sheetNames = workBook.SheetNames;
-      const ledgergroupData:Array<LedgerGroupImport> = xlsx.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
-      const ledgergroupi = await this.createLedgerGroup(ledgergroupData, uProfile, finYearRepository);
-      return ledgergroupi;
-
-    }
-
-
-  /**
-   * Get files and fields for the request
-   * @param request - Http request
-   */
-  private static getFilesAndFields(request: Request) {
-
-    const uploadedFiles = request.files;
-    const mapper = (file2: globalThis.Express.Multer.File) => ({
-      fieldname: file2.fieldname,
-      originalname: file2.originalname,
-      encoding: file2.encoding,
-      mimetype: file2.mimetype,
-      size: file2.size,
-    });
-    let files = [];
-    if (Array.isArray(uploadedFiles)) {
-
-      files = uploadedFiles.map(mapper);
-
-    } else {
-
-      for (const filename in uploadedFiles) {
-
-        if (!uploadedFiles.hasOwnProperty(filename)) {
-
-          continue;
-
-        }
-
-        files.push(...uploadedFiles[filename].map(mapper));
-
-      }
-
-    }
-    return {files,
-      fields: request.body};
+    const resp = await this.ledgerGroupService
+      .importLedgerGroup(request, response2, fileUploadHandler, uProfile, finYearRepository);
+    return resp;
 
   }
 
--- server/src/services/ledger-group.service.ts
+++ server/src/services/ledger-group.service.ts
@@ -1,14 +1,60 @@
-import {injectable, BindingScope} from '@loopback/core';
-import { Filter, repository } from '@loopback/repository';
+import {injectable, BindingScope, } from '@loopback/core';
+import { Count, Filter, FilterExcludingWhere, repository, Where, } from '@loopback/repository';
+import { Request, Response, HttpErrors } from '@loopback/rest';
 import { LedgerGroup } from '../models';
-import { LedgerGroupRepository } from '../repositories';
+import { FinYearRepository, LedgerGroupRepository } from '../repositories';
 import { LedgerGroup as LedgerGroupIntf } from '@shared/entity/accounting/ledger-group';
+import { LedgerGroupImport } from '../utils/ledgergroup-import-spec';
+import { FileUploadHandler } from '../types';
+import { ProfileUser } from './user.service';
+import { Save } from '../utils/save-spec';
+import xlsx from 'xlsx';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class LedgerGroupService {
 
   constructor(@repository(LedgerGroupRepository)
   public ledgerGroupRepository : LedgerGroupRepository,) {}
+
+
+  /**
+   * Get files and fields for the request
+   * @param request - Http request
+   */
+  private static getFilesAndFields(request: Request) {
+
+    const uploadedFiles = request.files;
+    const mapper = (file2: globalThis.Express.Multer.File) => ({
+      fieldname: file2.fieldname,
+      originalname: file2.originalname,
+      encoding: file2.encoding,
+      mimetype: file2.mimetype,
+      size: file2.size,
+    });
+    let files = [];
+    if (Array.isArray(uploadedFiles)) {
+
+      files = uploadedFiles.map(mapper);
+
+    } else {
+
+      for (const filename in uploadedFiles) {
+
+        if (!uploadedFiles.hasOwnProperty(filename)) {
+
+          continue;
+
+        }
+
+        files.push(...uploadedFiles[filename].map(mapper));
+
+      }
+
+    }
+    return {files,
+      fields: request.body};
+
+  }
 
   private lgWithParentsAggregate = [
     {
@@ -112,8 +158,120 @@ export class LedgerGroupService {
 
   }
 
-  findLedgerGroupsWithParents =
-  async(ldIds: Array<string>):Promise<Array<LedgerGroup>> => {
+  private fetchChilds = async(lgCodes: Array<string>):Promise<Array<LedgerGroup>> => {
+
+    const pQuery = await this.ledgerGroupRepository.execute(this.ledgerGroupRepository.modelClass.name, 'aggregate', [
+      {
+        '$graphLookup': {
+          'from': 'LedgerGroup',
+          'startWith': '$parentId',
+          'connectFromField': 'parentId',
+          'connectToField': '_id',
+          'as': 'parents',
+        }
+      },
+      {
+        '$addFields': {
+          'id': '$_id',
+        }
+      },
+      {
+        '$addFields': {
+          'parents': {
+            '$filter': {
+              'input': '$parents',
+              'cond': { '$in': [ '$$this.code', lgCodes ] }
+            }
+          }
+        }
+      },
+      {'$match': { '$or': [
+        {'parents': {'$exists': true,
+          '$not': {'$size': 0}}},
+        {'code': {'$in': lgCodes}}
+      ]}},
+    ]);
+
+
+    const lgsR = <Array<LedgerGroup>> await pQuery.toArray();
+    return lgsR;
+
+  }
+
+
+  private saveUploadedFile = (fileUploadHandler: FileUploadHandler, request: Request, response2: Response) =>
+    new Promise<Save>((resolve, reject) => {
+
+      fileUploadHandler(request, response2, (err: string) => {
+
+        if (err) {
+
+          reject(err);
+
+        } else {
+
+          resolve(LedgerGroupService.getFilesAndFields(request));
+
+        }
+
+
+      });
+
+    })
+
+
+    private createLedgerGroup = async(ledgergroupData:Array<LedgerGroupImport>,
+      uProfile: ProfileUser, finYearRepository : FinYearRepository)
+      :Promise<LedgerGroupImport[]> => {
+
+      const ledger:Array<LedgerGroupImport> = [];
+
+
+      for (const lgData of ledgergroupData) {
+
+
+        const code = lgData.Code;
+        const name = lgData.Name;
+        const parentCode = lgData.ParentCode;
+        const details = lgData.Details;
+        let parentId;
+        if (parentCode) {
+
+          const pLGroup = await this.ledgerGroupRepository.findOne({where: {code: parentCode}});
+          parentId = pLGroup?.id;
+
+        } else {
+
+          ledger.push(lgData);
+
+
+          continue;
+
+        }
+        const finYear = await finYearRepository.findOne({where: {code: {regexp: `/^${uProfile.finYear}$/i`}}});
+
+
+        if (!finYear) {
+
+          throw new HttpErrors.UnprocessableEntity('Please select a proper financial year.');
+
+
+        }
+
+        await this.ledgerGroupRepository.create({
+          code,
+          name,
+          parentId,
+          details,
+        });
+
+
+      }
+      return ledger;
+
+    }
+
+  findLedgerGroupsWithParents = async(ldIds: Array<string>):Promise<Array<LedgerGroup>> => {
 
     const aggregates = [ { '$match': { '_id': { '$in': ldIds } } }, ...this.lgWithParentsAggregate ];
     const pQuery = await this.ledgerGroupRepository.execute(this.ledgerGroupRepository.modelClass.name, 'aggregate', aggregates);
@@ -147,6 +305,108 @@ export class LedgerGroupService {
 
     const resp = await this.ledgerGroupRepository.find(filter);
     return resp;
+
+  }
+
+  create = async(ledgerGroup: Omit<LedgerGroup, 'id'>,
+  ): Promise<LedgerGroup> => {
+
+
+    const ledgergroupR = await this.ledgerGroupRepository.create(ledgerGroup);
+    return ledgergroupR;
+
+  }
+
+  count = async(where?: Where<LedgerGroup>): Promise<Count> => {
+
+    const countR = await this.ledgerGroupRepository.count(where);
+    return countR;
+
+  }
+
+  childs = async(where?: Where<LedgerGroup>,): Promise<LedgerGroup[]> => {
+
+    if (!where) {
+
+      throw new HttpErrors.BadRequest('Invalid parameter : LedgerGroup codes are required');
+
+    }
+    const whereC = where as {code: {inq: Array<string>}};
+    if (!whereC.code || !whereC.code.inq || whereC.code.inq.length < 1) {
+
+      throw new HttpErrors.BadRequest('Invalid parameter : LedgerGroup codes are required');
+
+    }
+    const lgsR = await this.fetchChilds(whereC.code.inq);
+
+
+    return lgsR;
+
+  }
+
+  updateAll = async(ledgerGroup: LedgerGroup, where?: Where<LedgerGroup>,): Promise<Count> => {
+
+    const countR = await this.ledgerGroupRepository.updateAll(ledgerGroup, where);
+    return countR;
+
+  }
+
+  findById = async(id: string, filter?: FilterExcludingWhere<LedgerGroup>): Promise<LedgerGroup> => {
+
+    const lgR = await this.ledgerGroupRepository.findById(id, filter);
+    return lgR;
+
+  }
+
+  updateById = async(id: string, ledgerGroup: LedgerGroup,): Promise<void> => {
+
+    await this.ledgerGroupRepository.updateById(id, ledgerGroup);
+
+  }
+
+  replaceById = async(id: string, ledgerGroup: LedgerGroup,): Promise<void> => {
+
+    await this.ledgerGroupRepository.replaceById(id, ledgerGroup);
+
+  }
+
+  deleteById = async(id: string): Promise<void> => {
+
+    await this.ledgerGroupRepository.deleteById(id);
+
+  }
+
+  deleteAll = async(where?: Where<LedgerGroup>,): Promise<Count> => {
+
+    if (!where) {
+
+      throw new HttpErrors.Conflict('Invalid parameter : LedgerGroup ids are required');
+
+    }
+    const whereC = where as {id: {inq: Array<string>}};
+    if (!whereC.id || !whereC.id.inq || whereC.id.inq.length < 1) {
+
+      throw new HttpErrors.Conflict('Invalid parameter : LedgerGroup ids are required');
+
+    }
+
+    const count = await this.ledgerGroupRepository.deleteAll(where);
+    return count;
+
+  }
+
+  async importLedgerGroup(request: Request, response2: Response, fileUploadHandler: FileUploadHandler,
+    uProfile: ProfileUser, finYearRepository : FinYearRepository,
+  ): Promise<LedgerGroupImport[]> {
+
+    await this.saveUploadedFile(fileUploadHandler, request, response2);
+    const [ fileDetails ] = request.files as Array<{path: string}>;
+    const savedFilePath = fileDetails.path;
+    const workBook = xlsx.readFile(savedFilePath);
+    const sheetNames = workBook.SheetNames;
+    const ledgergroupData:Array<LedgerGroupImport> = xlsx.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
+    const ledgergroupi = await this.createLedgerGroup(ledgergroupData, uProfile, finYearRepository);
+    return ledgergroupi;
 
   }
 
